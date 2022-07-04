7cdf560709ba0644cd0a1ba1b7563b672b1d2de8
FoliantR <s.nowgorodsky@yandex.ru>
2022-07-04 15:08:41 +0300

    Первый GUI

---
+++ Client.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigaGame
+{
+    internal class Client
+    {
+
+    }
+}
---
+++ CreateServer.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GigaGame
+{
+    /// <summary>
+    /// Логика взаимодействия для CreateServer.xaml
+    /// </summary>
+    public partial class CreateServer : Window
+    {
+        public CreateServer()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TcpListener serverSocket = new(IPAddress.Any, int.Parse(port.Text));
+            MessageBox.Show("Server started");
+            serverSocket.Start();
+
+            serverSocket.Stop();
+        }
+    }
+}
